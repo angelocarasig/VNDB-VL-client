6336e68b7ae69e745c3e556e8c54c42e9277d64b
angelocarasig <s3906344@student.rmit.edu.au>
2022-06-30 16:17:44 +1000

    changes

---
+++ build/precache-manifest.9ba879a5083dfa6620d9b2450c19a7f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "acefae7c9f4684012d87",
+    "url": "/static/css/main.f7b29a99.chunk.css"
+  },
+  {
+    "revision": "acefae7c9f4684012d87",
+    "url": "/static/js/main.acefae7c.chunk.js"
+  },
+  {
+    "revision": "36c65f266bee843a0e64",
+    "url": "/static/css/1.6ff56c93.chunk.css"
+  },
+  {
+    "revision": "36c65f266bee843a0e64",
+    "url": "/static/js/1.36c65f26.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d155a60c4a4f0d0c71b721e56cd144f5",
+    "url": "/static/media/banner.d155a60c.png"
+  },
+  {
+    "revision": "93bdfd6f74deaa3bd9c764341438ad1d",
+    "url": "/static/media/loading.93bdfd6f.gif"
+  },
+  {
+    "revision": "8ca973f608ab312d70a22f27bc6ac48b",
+    "url": "/static/media/icon.8ca973f6.jpg"
+  },
+  {
+    "revision": "908e9e3a22d8a3a0824fd0c50c00f4c4",
+    "url": "/index.html"
+  }
+];
